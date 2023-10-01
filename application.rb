13f1f38a3001a35bdd03dbb65afa0e8fe63c8de4
quentingirard74 <qgirard@nav-eco.fr>
2023-10-01 14:54:53 +0200

    feat: skill section in index

--- application.rb
+++ application.rb
@@ -6,8 +6,9 @@ class Application
     request = Rack::Request.new(env)
     case request.path
     when '/' then [200, HTML_HEADER, [render('index.erb')]]
+    when '/experiences' then [200, HTML_HEADER, [render('experiences.erb')]]
     else
-      [404, HTML_HEADER, ['<b>Not found</b>']]
+      [404, HTML_HEADER, [render('not_found.erb')]]
     end
   end
 
