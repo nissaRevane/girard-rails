e2c9c8c3699a702db6d5465640b2bbdd17cb25ac
nissaRevane <girard.quentin359@gmail.com>
2024-08-03 16:05:48 +0200

    parameterize experience card

--- controllers/experiences_controller.rb
+++ controllers/experiences_controller.rb
@@ -3,7 +3,18 @@
 require_relative 'application_controller'
 
 class ExperiencesController < ApplicationController
+  WORK_EXPERIENCES = [
+    { company_name: 'enerfip',
+      start_date: Date.new(2022, 6, 17),
+      end_date: Date.new(2024, 3, 15),
+      tags: ['Ruby on rails', 'PostgreSQL', 'Heroku', 'VueJs'] },
+    { company_name: 'shippingbo',
+      start_date: Date.new(2019, 10, 1),
+      end_date: Date.new(2022, 3, 16),
+      tags: ['Ruby on rails', 'PostgreSQL', 'Heroku', 'EmberJs'] },
+  ].freeze
+
   def self.index
-    [200, HTML_HEADER, [render('experiences.erb')]]
+    [200, HTML_HEADER, [render('experiences.erb', work_experiences: WORK_EXPERIENCES)]]
   end
 end
