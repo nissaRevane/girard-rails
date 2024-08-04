31d302f4d89c9467796f74aced1f4b043166ae67
nissaRevane <girard.quentin359@gmail.com>
2024-08-04 09:03:31 +0200

    make the paramater canonical for experiences urls

--- application.rb
+++ application.rb
@@ -6,11 +6,17 @@ require_relative 'controllers/home_controller'
 require_relative 'controllers/errors_controller'
 
 class Application
+  EXPERIENCED_COMPANY_REGEX = /^\/experiences\/(\w+)/
+
   def call(env)
     request = Rack::Request.new(env)
     case request.path
     when '/' then HomeController.index
     when '/experiences' then ExperiencesController.index
+    when EXPERIENCED_COMPANY_REGEX then
+      match_data = request.path.match(EXPERIENCED_COMPANY_REGEX)
+      company_name = match_data[1]
+      ExperiencesController.show(company_name)
     else
       ErrorsController.not_found
     end
--- controllers/experiences_controller.rb
+++ controllers/experiences_controller.rb
@@ -1,24 +1,46 @@
 # frozen_string_literal: true
 
+require 'date'
 require_relative 'application_controller'
 
 class ExperiencesController < ApplicationController
   WORK_EXPERIENCES = [
     { company_name: 'dickson',
-      start_date: Date.new(2024, 3, 16),
+      start_date: DateTime.new(2024, 3, 16),
       end_date: DateTime.now,
       tags: ['Ruby on rails', 'PostgreSQL', 'AWS', 'VueJs'] },
     { company_name: 'enerfip',
-      start_date: Date.new(2022, 6, 17),
-      end_date: Date.new(2024, 3, 15),
+      start_date: DateTime.new(2022, 6, 17),
+      end_date: DateTime.new(2024, 3, 15),
       tags: ['Ruby on rails', 'PostgreSQL', 'Heroku', 'VueJs'] },
     { company_name: 'shippingbo',
-      start_date: Date.new(2019, 10, 1),
-      end_date: Date.new(2022, 3, 16),
+      start_date: DateTime.new(2019, 10, 1),
+      end_date: DateTime.new(2022, 3, 16),
       tags: ['Ruby on rails', 'PostgreSQL', 'Heroku', 'EmberJs'] },
   ].freeze
 
   def self.index
-    [200, HTML_HEADER, [render_with_layout('experiences.erb', work_experiences: WORK_EXPERIENCES)]]
+    [
+      200,
+      HTML_HEADER,
+      [render_with_layout('experiences/index.html.erb', work_experiences: WORK_EXPERIENCES)]
+    ]
+  end
+
+  def self.show(company_name)
+    work_experience = WORK_EXPERIENCES.find { |exp| exp[:company_name] == company_name }
+    return ErrorsController.not_found unless work_experience
+    
+    [
+      200,
+      HTML_HEADER,
+      [render_with_layout(
+        'experiences/show.html.erb',
+        company_name: work_experience[:company_name],
+        start_date: work_experience[:start_date],
+        end_date: work_experience[:end_date],
+        tags: work_experience[:tags]
+      )]
+    ]
   end
 end
