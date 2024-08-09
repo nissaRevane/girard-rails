d18c78c1ff20e5b308974ea86b891ff12ab8e365
nissaRevane <girard.quentin359@gmail.com>
2024-08-09 18:19:32 +0200

    change skill image for a link collection

--- controllers/home_controller.rb
+++ controllers/home_controller.rb
@@ -3,7 +3,15 @@
 require_relative 'application_controller'
 
 class HomeController < ApplicationController
+  SKILLS = %w[
+    rails
+    database
+    test
+    cloud
+    async
+    frontend
+  ]
   def self.index
-    [200, HTML_HEADER, [render_with_layout('index.erb')]]
+    [200, HTML_HEADER, [render_with_layout('home.html.erb', skills: SKILLS)]]
   end
 end
